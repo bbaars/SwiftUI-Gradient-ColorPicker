1cd121f1c49149380c86d14c0c997dce6d2d551d
Brandon Baars <brandonbaars@gmail.com>
2020-01-12 23:32:52 -0500

    Revert "Added color picker with animations."
    This reverts commit 56172e6a7fb5ce47d7377fafc284e92ccfcfa820.

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -9,19 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    // 1
-    @State var currentColor: Color = .clear
-    
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            // 2
-            currentColor
-            
-            // 3
-            ColorPickerView(chosenColor: $currentColor)
-                .frame(width: 50, height: 200)
-                .offset(x: 0, y: 75)
-        }.edgesIgnoringSafeArea(.all) // fill past all edges on iPhoneX
+        Text("Hello, World!")
     }
 }
 
